a6eb44647bf44668b06267d188accac2e847e880
Zizo95 <zizo.brown.mbiko@gmail.com>
2024-05-06 15:59:07 +0200

    updated html

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const messageEl = document.querySelector(".message");
 const button = document.querySelector(".subscribe-btn");
 const main = document.querySelector("main");
 const section = document.querySelector("section");
-const mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const mailInput = document.querySelector("#text1");
 
-button.addEventListener("click",(inputText) => {
-  if(inputText.value.match(mailformat)){
-    document.form1.text1.focus();
+
+
+function mailValidation () {
+  const mailValue = mailInput.value.trim();
+  if(mailFormat.test(mailValue)){
 section.style.display = "block";
 main.style.display = "none";
-    return true;
-
   }
 else{
-  document.form1.text1.focus();
-messageEl.style.display = "block";
-return false
+messageEl.style.display = "inline-block";
 }
-})
+}
+button.addEventListener("click", mailValidation);
